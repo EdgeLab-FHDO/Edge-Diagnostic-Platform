979fe1fd6dff8a1411bbfd44d4cdbac8847e1c57
juan-castrillon <castrillon.95@gmail.com>
2020-08-03 15:07:21 +0200

    Added Test file using junit

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,2 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Tester {
+    SimpleApp app = new SimpleApp();
+    @Test
+    public void NullTest(){
+        Assert.assertNotNull(app.sayHello());
+    }
+    @Test
+    public void ContentTest() {
+        Assert.assertEquals("Wrong Message","Hello World!", app.sayHello());
+    }
 }
