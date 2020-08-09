1dbe51219900c9c01bfd5805e2dffe08efbff620
juan-castrillon <castrillon.95@gmail.com>
2020-08-09 17:21:42 +0200

    Added loop in Main

--- src/main/java/InfrastructureManager/Master.java
+++ src/main/java/InfrastructureManager/Master.java
@@ -2,22 +2,14 @@ package InfrastructureManager;
 
 public class Master {
     private final CommandSet commandSet;
-    private MasterInput input;
-    private MasterOutput output;
+    private final MasterInput input;
+    private final MasterOutput output;
 
     public Master() {
         commandSet = CommandSet.getInstance();
         input =new ConsoleInput();
         output = new ConsoleOutput();
     }
-
-    public static void main(String[] args) {
-        Master master = new Master();
-        String in = master.fromInput();
-        String mapping = master.execute(in);
-        master.toOutput(mapping);
-    }
-
     public String fromInput() {
         return this.input.read();
     }
@@ -26,6 +18,18 @@ public class Master {
     }
     public void toOutput(String response) {
         this.output.out(response);
+    }
+
+    public static void main(String[] args) {
+        Master master = new Master();
+        String in;
+        String mapping;
+        while (true) {
+            in = master.fromInput();
+            mapping = master.execute(in);
+            master.toOutput(mapping);
+            if (in.equals("exit")) {break;}
+        }
     }
 
 }
