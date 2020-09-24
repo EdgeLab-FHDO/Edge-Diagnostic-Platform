55ee3e705cba07dc5162de4fdce76318c573f200
juan-castrillon <castrillon.95@gmail.com>
2020-09-24 13:50:59 +0200

    Everything JSON with only one Library (Jackson)

--- src/main/java/InfrastructureManager/Rest/RestOutput.java
+++ src/main/java/InfrastructureManager/Rest/RestOutput.java
@@ -4,6 +4,7 @@ import InfrastructureManager.Master;
 import InfrastructureManager.MasterOutput;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -13,37 +14,35 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
-import org.json.*;
-
 public class RestOutput implements MasterOutput {
 
     private static RestOutput instance = null;
 
     private final Queue<String> output;
     private final Map<String,Double> limitNodes;
+    private final ObjectMapper mapper;
 
     public Route sendLimitInfo = (Request request, Response response) -> getLimitInfo(response);
     public Route getNode = (Request request, Response response) -> printResponse();
 
     private RestOutput() {
+        this.mapper = new ObjectMapper();
         this.output = new LinkedList<>();
         this.limitNodes = new LinkedHashMap<>();
     }
-    public JSONObject printResponse() {
-        JSONObject response = new JSONObject();
-
+    public ObjectNode printResponse() {
+        ObjectNode response_body = mapper.createObjectNode();
         if(output.isEmpty()) {
-            response.put("content", "");
+            response_body.put("content", "");
         } else {
-            response.put("content", output.remove());
+            response_body.put("content", output.remove());
         }
-
-        return response;
+        return response_body;
     }
+
     public String getLimitInfo(Response response) {
-        ObjectMapper mapper = new ObjectMapper();
-        String response_body = "";
         response.type("application/json");
+        String response_body = "";
         try {
             response_body = mapper.writeValueAsString(limitNodes);
         } catch (JsonProcessingException e) {
