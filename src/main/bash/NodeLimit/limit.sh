e7d5a89e2fc135b2f4564202d07f61cbff9141fd
juan-castrillon <castrillon.95@gmail.com>
2020-09-27 15:47:32 +0200

    Added parameter processing and verbose mode

--- src/main/bash/NodeLimit/limit.sh
+++ src/main/bash/NodeLimit/limit.sh
@@ -4,22 +4,43 @@
 
 no_quotes_regex='"\K\w+(?=")' ##Regular expression to match a word between quotes, ignoring the quotes
 read_data="{}" #Start with the empty response from the master (no command given yet)
-if [[ -n "$1" ]] #If there is an argument (URL) passed
+request_address=
+verbose=0
+
+while [[ -n "$1" ]] ##Process the passed parameters
+do
+    case "$1" in
+    -v) ##Control verbose output with the -v flag
+      verbose=1
+      ;;
+    -a) ## Read the Request URL after the -a flag
+      shift
+      request_address="$1"
+    esac
+    shift
+done
+
+
+if [[ -n "$request_address" ]] #If there is an URL passed
 then
   while :
   do
-      curl -s "$1" | grep -Po '"\w+":"\w+"' > limitFile #Do the GET request and save the data to a file
+      curl -s "$request_address" | grep -Po '"\w+":"\w+"' > limitFile #Do the GET request and save the data to a file
       if [[ "$read_data" != $(cat limitFile) ]] #Only process if there is new data
       then
+        [[ $verbose -eq 1 ]] && echo "Processing new data available"
         while IFS=: read -r node_tag time_values #For each line of the file
         do
           node_tag=$(grep -Po "$no_quotes_regex" <<< "$node_tag") #Get rid of quotes
           time_values=$(grep -Po "$no_quotes_regex" <<< "$time_values") #Get rid of quotes
           IFS=_ read -r quota_us period_us <<< "$time_values" #Split both time values based on "_" separator
 
-          echo "Read tag: $node_tag"
-          echo "Read Quota: $quota_us"
-          echo "Read Period: $period_us"
+          if [[ $verbose -eq 1 ]]
+          then
+            echo "Read tag: $node_tag"
+            echo "Read Quota: $quota_us"
+            echo "Read Period: $period_us"
+          fi
 
           #To limit the number of cores assigned cfs_period and cfs_quota have to be set
           # number of cores = cfs_quota_us / cfs_period_us (e.g. 0.5 core = 50000 / 100000, 2 cores = 200000 / 100000)
@@ -38,7 +59,7 @@ then
       fi
   done
 else
-  echo "Master REST server URL missing, pass the URL as a parameter to the script"
+  echo "Master REST server URL missing, pass the URL as a parameter to the script with the -a flag"
 fi
 
 
