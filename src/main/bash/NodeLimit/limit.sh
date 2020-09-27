57ec1b10931826daf15a1778f976ecff0f56f389
juan-castrillon <castrillon.95@gmail.com>
2020-09-27 16:15:37 +0200

    Process assigment out of condition

--- src/main/bash/NodeLimit/limit.sh
+++ src/main/bash/NodeLimit/limit.sh
@@ -4,6 +4,7 @@
 
 no_quotes_regex='"\K\w+(?=")' ##Regular expression to match a word between quotes, ignoring the quotes
 read_data="{}" #Start with the empty response from the master (no command given yet)
+first_data=0 #To be "turned on" only when the loop has processed data for the first time
 request_address=
 verbose=0
 
@@ -28,6 +29,7 @@ then
       curl -s "$request_address" | grep -Po '"\w+":"\w+"' > limitFile #Do the GET request and save the data to a file
       if [[ "$read_data" != $(cat limitFile) ]] #Only process if there is new data
       then
+        [[ $first_data -eq 0 ]] && first_data=1 ##The first time the data flag is turned on
         [[ $verbose -eq 1 ]] && echo "Processing new data available"
         while IFS=: read -r node_tag time_values #For each line of the file
         do
@@ -48,15 +50,19 @@ then
           sudo cgset -r cpu.cfs_period_us="$period_us" "$node_tag"
           sudo cgset -r cpu.cfs_quota_us="$quota_us" "$node_tag"
 
-          #Assignment of the processes to the group based on the PID
-          #First command in the pipe (ps) outputs all running processes (-e), in full format(-f) and the threads (-T) with SPID
-          #AWK command, returns the SPIDs of the process which were called with the node_tag as argument
-          # Xargs turns the output of awk into arguments for cgclassify, which then assigns the processes to the limited group
-          ps -ef -T | awk -v tag="$node_tag" '!/awk/ && $0 ~ tag { print $3 }' | xargs -t sudo cgclassify -g cpu:/"$node_tag"
-
         done < limitFile
         read_data=$(cat limitFile) #Save processed content
       fi
+
+      if [[ $first_data -eq 1 ]] #Verifies and assign processes only if there is at least one cgroup created (data was processed once)
+      then
+        #Assignment of the processes to the group based on the PID
+        #First command in the pipe (ps) outputs all running processes (-e), in full format(-f) and the threads (-T) with SPID
+        #AWK command, returns the SPIDs of the process which were called with the node_tag as argument
+        # Xargs turns the output of awk into arguments for cgclassify, which then assigns the processes to the limited group
+        ps -ef -T | awk -v tag="$node_tag" '!/awk/ && $0 ~ tag { print $3 }' | xargs -t sudo cgclassify -g cpu:/"$node_tag"
+      fi
+      sleep 1 #Delay of 1s to the infinite loop
   done
 else
   echo "Master REST server URL missing, pass the URL as a parameter to the script with the -a flag"
