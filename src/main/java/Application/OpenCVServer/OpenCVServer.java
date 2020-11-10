8a0c1da16eed763f6c65e8e4b5ade6d90ce71f6f
spvmastern <theodore.lumaksono@yahoo.com>
2020-11-10 02:11:50 +0100

    Fix OpenCVServer error

--- src/main/java/Application/OpenCVServer/OpenCVServer.java
+++ src/main/java/Application/OpenCVServer/OpenCVServer.java
@@ -130,7 +130,7 @@ public class OpenCVServer {
         String missingParameterList = "";
 
         for(int i=0; i<args.length; i++) {
-            System.out.println(argument[1]);
+            argument = args[i].split("=");
             switch(argument[0]) {
                 case "SERVER_ID":
                     serverId = argument[1];
