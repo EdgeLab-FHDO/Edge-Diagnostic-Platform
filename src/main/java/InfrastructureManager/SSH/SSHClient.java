194f69e57b1059c9129885c96481bd1b5b408c03
juan-castrillon <castrillon.95@gmail.com>
2020-11-25 15:37:09 +0100

    Works with VM

--- src/main/java/InfrastructureManager/SSH/SSHClient.java
+++ src/main/java/InfrastructureManager/SSH/SSHClient.java
@@ -1,11 +1,21 @@
 package InfrastructureManager.SSH;
 
 import InfrastructureManager.MasterOutput;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
 
 public class SSHClient extends MasterOutput {
 
-    private JSch jsch;
+    private final JSch jsch;
+    private final String username = "jpcr3108";
+    private final String host = "192.168.0.127";
+    private final String password = "jp1234";
+    private final int port = 22;
+
 
     public SSHClient(String name) {
         super(name);
@@ -31,6 +41,35 @@ public class SSHClient extends MasterOutput {
     }
 
     private void execute(String command) {
-        System.out.println("To execute remotely: " + command);
+        Session session = null;
+        ChannelExec channel = null;
+        try {
+            session = jsch.getSession(username,host,port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+
+            while (channel.isConnected()) {
+                Thread.sleep(100);
+            }
+
+            String responseString = new String(responseStream.toByteArray());
+            System.out.println(responseString);
+        } catch (JSchException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
     }
 }
