d9a4cd5102f8a38aeceb5b17224b04a0613e0ae0
juan-castrillon <castrillon.95@gmail.com>
2020-12-02 13:43:15 +0100

    Added default UTF-8 encoding and support for others

--- src/main/java/InfrastructureManager/FileOutput/FileOutput.java
+++ src/main/java/InfrastructureManager/FileOutput/FileOutput.java
@@ -3,11 +3,17 @@ package InfrastructureManager.FileOutput;
 import InfrastructureManager.MasterOutput;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class FileOutput extends MasterOutput {
 
+    private Charset encoding;
+
     public FileOutput(String name) {
         super(name);
+        this.encoding = StandardCharsets.UTF_8;
     }
 
     @Override
@@ -16,8 +22,10 @@ public class FileOutput extends MasterOutput {
         if (command[0].equals("file_out")) { //The commands must come like "file_out command"
             try {
                 switch (command[1]) {
+                    case "encoding" :
+                        setEncoding(command[2]);
                     case "create":
-                        writeToFile(command[2], command[3]);
+                        writeToFile(command[2], restOfCommand(3, command));
                         break;
                     default:
                         throw new IllegalArgumentException("Invalid command for FileOutput");
@@ -28,13 +36,21 @@ public class FileOutput extends MasterOutput {
         }
     }
 
+    public void setEncoding(String encodingName) {
+        this.encoding = Charset.forName(encodingName);
+    }
+
     private void writeToFile(String path, String content) {
         File file = new File(path);
         try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
-            byte[] toWrite = content.getBytes();
+            byte[] toWrite = content.getBytes(this.encoding);
             out.write(toWrite);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String restOfCommand(int position, String[] command) {
+        return String.join(" ",Arrays.copyOfRange(command,position,command.length));
     }
 }
