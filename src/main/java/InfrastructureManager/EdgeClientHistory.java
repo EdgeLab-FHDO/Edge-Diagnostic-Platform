db858836ed42ca17ebda539a8ec941bade5e3d8b
zero212 <zerovnb@gmail.com>
2020-12-15 16:11:27 +0100

    #63 remove getter for historyMap and connectedMap, throw exception instead of return null

--- src/main/java/InfrastructureManager/EdgeClientHistory.java
+++ src/main/java/InfrastructureManager/EdgeClientHistory.java
@@ -14,9 +14,13 @@ import java.util.*;
  * Also set historyMap and set ConnectedMap
  */
 public class EdgeClientHistory {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private String id;
+    //multimap with client id - nodeID,history score
+    private final Multimap<String, HashMap<String, Long>> historyMap = ArrayListMultimap.create();
+    //multimap with client id - nodeID,connected time
+    private final Multimap<String, HashMap<String, Long>> connectedMap = ArrayListMultimap.create();
 
-    // - -------------------------------- ---- THIS IS THE CONSTRUCTOR -----------------------------
+
     public EdgeClientHistory(String id) {
         this.id = id;
     }
@@ -31,17 +35,15 @@ public class EdgeClientHistory {
         this.id = id;
     }
 
-    public Multimap<String, HashMap<String, Long>> getHistoryMap() {
-        return historyMap;
-    }
 
     /**
      * Assign client, node and history score into class's multimap.
      * This function create a new HashMap, use mainly for initialize out collection.
      * Don't use to replace value, because we are using multimap, 1 key can have several similar value
-     * @param clientID
-     * @param nodeID
-     * @param nodeHistory
+     *
+     * @param clientID    client's string ID
+     * @param nodeID      node ID
+     * @param nodeHistory history score between client and nodes
      */
     public void setHistoryMap(String clientID, String nodeID, Long nodeHistory) {
         HashMap<String, Long> nodeAndScore = new HashMap<>();
@@ -49,12 +51,12 @@ public class EdgeClientHistory {
         this.historyMap.put(clientID, nodeAndScore);
     }
 
-    public void setHistoryScoreForClient(String clientID, String nodeID, Long nodeHistoryScore){
+    public void setHistoryScoreForClient(String clientID, String nodeID, Long nodeHistoryScore) {
         Collection<HashMap<String, Long>> clientHistoryMapList = this.historyMap.get(clientID);
-        for (HashMap<String, Long> theList : clientHistoryMapList){
-            if (theList.containsKey(nodeID)){
+        for (HashMap<String, Long> theList : clientHistoryMapList) {
+            if (theList.containsKey(nodeID)) {
                 //replace with new score
-                theList.put(nodeID,nodeHistoryScore);
+                theList.put(nodeID, nodeHistoryScore);
                 break;
             }
         }
@@ -66,9 +68,9 @@ public class EdgeClientHistory {
      * This function create a new HashMap, use mainly for initialize out collection.
      * Don't use to replace value, because we are using multimap, 1 key can have several similar value
      *
-     * @param clientID
-     * @param nodeID
-     * @param connectedTime
+     * @param clientID      client's ID in String
+     * @param nodeID        node's ID in string
+     * @param connectedTime connect time between client and node
      */
     public void setConnectedMap(String clientID, String nodeID, Long connectedTime) {
         HashMap<String, Long> nodeAndTime = new HashMap<>();
@@ -76,16 +78,24 @@ public class EdgeClientHistory {
         this.connectedMap.put(clientID, nodeAndTime);
     }
 
-    public void setConnectedTimeForClient (String clientID, String nodeID, Long connectedTime) {
+    public void setConnectedTimeForClient(String clientID, String nodeID, Long connectedTime) {
         Collection<HashMap<String, Long>> clientConnectedMapList = this.connectedMap.get(clientID);
-        for (HashMap<String, Long> theList : clientConnectedMapList ){
-            if (theList.containsKey(nodeID)){
+        for (HashMap<String, Long> theList : clientConnectedMapList) {
+            if (theList.containsKey(nodeID)) {
                 //replace with new connected time
                 theList.put(nodeID, connectedTime);
                 break;
             }
         }
     }
+
+    /**
+     * Get time client's last connected to node
+     *
+     * @param clientID client's ID in String
+     * @param nodeID   node's ID in string
+     * @return the time client last connected to node in long
+     */
 
     public Long getConnectedTime(String clientID, String nodeID) {
         Long connectedTime = Long.MAX_VALUE;
@@ -94,17 +104,20 @@ public class EdgeClientHistory {
         Collection<HashMap<String, Long>> nodeAndTimeList = this.connectedMap.get(clientID);
 
         //get the correct nodeAndTime coupling
-        for (HashMap<String,Long> nodeAndTime: nodeAndTimeList) {
-            if (nodeAndTime.containsKey(nodeID)){
+        for (HashMap<String, Long> nodeAndTime : nodeAndTimeList) {
+            if (nodeAndTime.containsKey(nodeID)) {
                 //Get connected time
                 connectedTime = nodeAndTime.get(nodeID);
                 break;
             }
         }
 
-        if (connectedTime >= Long.MAX_VALUE){
-            logger.error("can't find connected time in the map.");
-            return null;
+        if (connectedTime >= Long.MAX_VALUE) {
+            try {
+                throw new Exception("can't find connected time in the map.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return connectedTime;
@@ -112,9 +125,10 @@ public class EdgeClientHistory {
 
     /**
      * Get history score from client and node ID.
-     * @param clientID
-     * @param nodeID
-     * @return history score
+     *
+     * @param clientID client's ID in string
+     * @param nodeID   node's ID in string
+     * @return history score of node to client
      */
 
     public Long getHistoryScore(String clientID, String nodeID) {
@@ -123,41 +137,32 @@ public class EdgeClientHistory {
         Collection<HashMap<String, Long>> nodeAndScoreList = this.historyMap.get(clientID);
 
         //get the correct nodeAndScore coupling
-        for (HashMap<String,Long> nodeAndScore : nodeAndScoreList){
-            if (nodeAndScore.containsKey(nodeID)){
+        for (HashMap<String, Long> nodeAndScore : nodeAndScoreList) {
+            if (nodeAndScore.containsKey(nodeID)) {
                 //get history score
                 historyScore = nodeAndScore.get(nodeID);
                 break;
             }
         }
 
-        if (historyScore >= Long.MAX_VALUE){
-            logger.error("Can't find the history's score in the map");
-            return null;
+        if (historyScore >= Long.MAX_VALUE) {
+            try {
+                throw new Exception("Can't find the history's score in the map");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return historyScore;
     }
 
-    public Multimap<String, HashMap<String, Long>> getConnectedMap() {
-        return connectedMap;
-    }
 
     @Override
-    public String toString(){
-        return    "Client         - [" + id + "]\n"
+    public String toString() {
+        return "Client         - [" + id + "]\n"
                 + "score          - " + historyMap + "\n"
                 + "connected time - " + connectedMap;
     }
 
-
-    private String id;
-    //multimap with client id - nodeID,history score
-    private Multimap<String, HashMap<String, Long>> historyMap = ArrayListMultimap.create();
-    //multimap with client id - nodeID,history score
-    private Multimap<String, HashMap<String, Long>> connectedMap = ArrayListMultimap.create();
-
-
-
-
 }
+
