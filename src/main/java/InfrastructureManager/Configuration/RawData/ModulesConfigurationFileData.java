b7bb31bc850c4e8985545acade73fe48672ee66c
juan-castrillon <castrillon.juan95@gmail.com>
2021-02-02 11:11:39 +0100

    Commented and documented files in Configuration package

--- src/main/java/InfrastructureManager/Configuration/RawData/ModulesConfigurationFileData.java
+++ src/main/java/InfrastructureManager/Configuration/RawData/ModulesConfigurationFileData.java
@@ -7,24 +7,37 @@ import java.util.List;
 
 
 /**
- * Configuration Data Object (Mapped from JSON Config File)
+ * Configuration Data Object (Mapped from JSON Config File). Is basically the POJO representation of the
+ * configuration file.
  */
 public class ModulesConfigurationFileData {
 
     private final List<ModuleConfigData> modules;
     private final List<ConnectionConfigData> connections;
 
+
+    /**
+     * Constructor of the class, normally called by the {@link com.fasterxml.jackson.databind.ObjectMapper}
+     * that parses the configuration file from JSON to an object of this class. Starts all fields in null.
+     */
     public ModulesConfigurationFileData() {
-        //Initialize all values in null;
         modules = null;
         connections = null;
     }
 
+    /**
+     * Get the defined connections in their raw representation
+     * @return A List of {@link ConnectionConfigData} objects.
+     */
     public List<ConnectionConfigData> getConnections() {
         return connections;
     }
 
 
+    /**
+     * Get the defined modules in their raw representation
+     * @return A List of {@link ModuleConfigData} objects.
+     */
     public List<ModuleConfigData> getModules() {
         return modules;
     }
