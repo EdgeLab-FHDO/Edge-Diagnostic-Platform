51f887048e5a764ee7f14da9b939124151efccd8
juan-castrillon <castrillon.juan95@gmail.com>
2021-02-18 13:55:22 +0100

    Documented ConsoleModule

--- src/main/java/InfrastructureManager/Modules/Console/ConsoleModule.java
+++ src/main/java/InfrastructureManager/Modules/Console/ConsoleModule.java
@@ -4,13 +4,32 @@ package InfrastructureManager.Modules.Console;
 import InfrastructureManager.ModuleManagement.PlatformModule;
 import InfrastructureManager.ModuleManagement.RawData.ModuleConfigData;
 
+/**
+ * {@link PlatformModule} that adds user interacting functionality to the platform.
+ *
+ * The console module, provides the necessary inputs and outputs to interact with the console (terminal),
+ * which means read from standard input and print to the standard output.
+ *
+ * Provides one console input hardcoded with the name ".in"
+ * And one console output with the name ".out"
+ *
+ * @see <a href="https://github.com/EdgeLab-FHDO/Edge-Diagnostic-Platform/wiki/Console-Module">Wiki Entry</a>
+ */
 public class ConsoleModule extends PlatformModule {
 
 
+    /**
+     * Create a new console module. The module created is not initialized and cannot be used
+     */
     public ConsoleModule() {
         super();
     }
 
+    /**
+     * Based on raw module data, configures the module to use it. In this case, extracts the name and creates the input and
+     * output with their predefined names.
+     * @param data Raw module data.
+     */
     @Override
     public void configure(ModuleConfigData data) {
         this.setName(data.getName());
--- src/main/java/InfrastructureManager/Modules/Console/ConsoleModuleObject.java
+++ src/main/java/InfrastructureManager/Modules/Console/ConsoleModuleObject.java
@@ -3,11 +3,18 @@ package InfrastructureManager.Modules.Console;
 import InfrastructureManager.ModuleManagement.ImmutablePlatformModule;
 import InfrastructureManager.PlatformObject;
 
+/**
+ * Concrete implementation of {@link PlatformObject} for the Console Module.
+ *
+ * All objects (input and outputs included) of the module must extend this class.
+ */
 public class ConsoleModuleObject extends PlatformObject {
-    public ConsoleModuleObject(ImmutablePlatformModule ownerModule) {
-        super(ownerModule);
-    }
 
+    /**
+     * Creates a new ConsoleModuleObject
+     * @param ownerModule Owner module of this object
+     * @param name Name of the object
+     */
     public ConsoleModuleObject(ImmutablePlatformModule ownerModule, String name) {
         super(ownerModule, name);
     }
