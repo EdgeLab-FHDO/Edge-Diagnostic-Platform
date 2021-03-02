5f55ddb48dbc40ba469263962ecbbf186d54f9f1
juan-castrillon <castrillon.juan95@gmail.com>
2021-03-02 18:51:31 +0100

    Created app structure for easy deployment

---
+++ src/main/java/EdgeDiagnosticsApp/Client/DiagnosticsClient.java
@@ -0,0 +1,7 @@
+package EdgeDiagnosticsApp.Client;
+
+public class DiagnosticsClient {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Client");
+    }
+}
