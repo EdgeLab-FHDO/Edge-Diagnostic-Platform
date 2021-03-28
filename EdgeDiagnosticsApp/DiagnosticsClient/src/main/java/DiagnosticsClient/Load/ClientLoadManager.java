d2ec45fa6d29ca7447d11d6c078f2faa36db7d32
juan-castrillon <castrillon.juan95@gmail.com>
2021-03-28 19:35:35 +0200

    Added formatting to latency report

--- EdgeDiagnosticsApp/DiagnosticsClient/src/main/java/DiagnosticsClient/Load/ClientLoadManager.java
+++ EdgeDiagnosticsApp/DiagnosticsClient/src/main/java/DiagnosticsClient/Load/ClientLoadManager.java
@@ -10,9 +10,11 @@ import LoadManagement.BasicLoadManager;
 import Communication.Exception.RESTClientException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ClientLoadManager extends BasicLoadManager {
 
@@ -21,11 +23,15 @@ public class ClientLoadManager extends BasicLoadManager {
     private Map<Integer, Long> latencyMeasurements;
     private final ClientPlatformConnection connection;
     private ClientSocketOptions options;
+    private String reportFilePath;
+    private int fileCounter;
 
     public ClientLoadManager(ClientPlatformConnection connection, ServerInformation serverInformation) {
         this.address = serverInformation.getIpAddress();
         this.port = serverInformation.getPort();
         this.connection = connection;
+        this.reportFilePath = "";
+        this.fileCounter = 0;
     }
 
     public void setSocketOptions(ClientSocketOptions options) {
@@ -36,12 +42,19 @@ public class ClientLoadManager extends BasicLoadManager {
         LoadSender sender = getSender();
         if (this.options != null) sender.changeSocketConfiguration(this.options);
         sender.send(load);
+        reportFilePath = "load" + fileCounter + ".txt";
+        fileCounter++;
     }
 
-    public void reportMeasurements() throws JsonProcessingException, RESTClientException {
+    public void reportMeasurements() throws RESTClientException, JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        String latencyString = mapper.writeValueAsString(latencyMeasurements);
-        connection.sendMeasurements(latencyString);
+        ObjectNode result = mapper.createObjectNode();
+
+        String latencyString = formatMeasurements();
+        result.put("path","LatencyReports/" + reportFilePath);
+        result.put("content", latencyString);
+        String body = mapper.writeValueAsString(result);
+        connection.sendMeasurements(body);
     }
 
     private LoadSender getSender() {
@@ -50,6 +63,12 @@ public class ClientLoadManager extends BasicLoadManager {
             case TCP -> new TCPLoadSender(address,port,latencyMeasurements);
             case UDP -> new UDPLoadSender(address,port,latencyMeasurements);
         };
+    }
+
+    private String formatMeasurements() {
+        return latencyMeasurements.entrySet().stream()
+                .map(e -> e.toString().replaceAll("=",",").trim())
+                .collect(Collectors.joining(";"));
     }
 
 
