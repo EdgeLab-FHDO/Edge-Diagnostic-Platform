27b479449119be1c2e6cfb865baa580f3ac40aee
juan-castrillon <castrillon.juan95@gmail.com>
2021-04-26 14:41:57 +0200

    Added reconnection logic in TCP client

--- EdgeDiagnosticsApp/DiagnosticsClient/src/main/java/DiagnosticsClient/Load/TCP/TCPLoadSender.java
+++ EdgeDiagnosticsApp/DiagnosticsClient/src/main/java/DiagnosticsClient/Load/TCP/TCPLoadSender.java
@@ -11,6 +11,7 @@ import DiagnosticsClient.Load.LoadTypes.PingLoad;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.List;
 
 import static java.net.StandardSocketOptions.IP_TOS;
@@ -19,6 +20,7 @@ import static java.net.StandardSocketOptions.SO_LINGER;
 public class TCPLoadSender extends LoadSender {
 
     private TCPClientSocketOptions socketConfig;
+    private static final int MAX_RECONNECTION_ATTEMPTS = 3;
 
     public TCPLoadSender(String address, int port, List<Double> latencyMeasurements) {
         super(address, port,latencyMeasurements);
@@ -55,30 +57,47 @@ public class TCPLoadSender extends LoadSender {
         int dataLength = pingMessage.length;
         int times = load.getTimes();
         int interval = load.getInterval_ms();
+        int connectionAttempts = 0;
         if (dataLength > 255 | dataLength < 1) throw new TCPConnectionException("Invalid length for ping message");
         String screenMessage = "Pinging " + address + ":" + port + " with " +
                 dataLength + " bytes of data " + times + " times. Protocol TCP";
-        try (
-                Socket clientSocket = new Socket(address,port);
-                BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream(),
-                        bufferInformation.getOutputStreamBuffer());
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()),
-                        bufferInformation.getInputReaderBuffer())
-        ) {
-            configureSocket(clientSocket);
-            //printSocketOptions(clientSocket);
-            System.out.println(screenMessage);
-            long[] latencies = new long[times];
-            for (int i = 0; i < times; i++) {
-                latencies[i] = singlePing(pingMessage,out,in);
-                Thread.sleep(interval);
+        do {
+            try (
+                    Socket clientSocket = new Socket(address,port);
+                    BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream(),
+                            bufferInformation.getOutputStreamBuffer());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()),
+                            bufferInformation.getInputReaderBuffer())
+            ) {
+                configureSocket(clientSocket);
+                //printSocketOptions(clientSocket);
+                System.out.println(screenMessage);
+                long[] latencies = new long[times];
+                for (int i = 0; i < times; i++) {
+                    latencies[i] = singlePing(pingMessage,out,in);
+                    Thread.sleep(interval);
+                }
+                System.out.println("Ping load test finished");
+                double avgLatency = this.addAverageLatency(latencies);
+                System.out.println("Average latency: " + avgLatency + " ns. Data size =" + times);
+                break;
+            } catch(SocketException e) {
+                connectionAttempts++;
+                if (connectionAttempts <= MAX_RECONNECTION_ATTEMPTS){
+                    System.out.println("Trying again");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException ex) {
+                        throw new TCPConnectionException("Interrupted while reconnecting", ex);
+                    }
+                    continue;
+                }
+                throw new TCPConnectionException("Server cannot be reached", e);
             }
-            System.out.println("Ping load test finished");
-            double avgLatency = this.addAverageLatency(latencies);
-            System.out.println("Average latency: " + avgLatency + " ns. Data size =" + times);
-        } catch (IOException | InterruptedException e) {
-            throw new TCPConnectionException("TCP Connection failed: ",e);
-        }
+            catch (IOException | InterruptedException e) {
+                throw new TCPConnectionException("TCP Connection failed: ",e);
+            }
+        } while (true);
     }
 
     private long singleFile(File load, BufferedOutputStream out, BufferedReader in,
@@ -108,33 +127,50 @@ public class TCPLoadSender extends LoadSender {
         File fileToSend = load.getFileToSend();
         long fileSize = load.getDataLength();
         String fileName = fileToSend.getName();
-        try (
-                Socket clientSocket = new Socket(address, port);
-                BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream(),
-                        bufferInformation.getOutputStreamBuffer());
-                DataOutputStream out2 = new DataOutputStream(clientSocket.getOutputStream());
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()),
-                        bufferInformation.getFileInputBuffer())
-        ){
-            configureSocket(clientSocket);
-            System.out.println("Sending file " + fileName + "(" + fileSize + " bytes) to " + address
-                    + ":" + port + ". Protocol TCP");
-            out2.writeInt((int) fileSize); //Send file size first as a 32bit number (Max size 2GB)
-            int fileBuffer = bufferInformation.getFileReadingBuffer();
-            int fileInputBuffer = bufferInformation.getFileInputBuffer();
-            long[] latencies = new long[times];
-            for (int i = 0; i < times; i++) {
-                latencies[i] = singleFile(fileToSend,out,in, fileBuffer, fileInputBuffer);
-                Thread.sleep(interval);
+        int connectionAttempts = 0;
+        do {
+            try (
+                    Socket clientSocket = new Socket(address, port);
+                    BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream(),
+                            bufferInformation.getOutputStreamBuffer());
+                    DataOutputStream out2 = new DataOutputStream(clientSocket.getOutputStream());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()),
+                            bufferInformation.getFileInputBuffer())
+            ){
+                configureSocket(clientSocket);
+                System.out.println("Sending file " + fileName + "(" + fileSize + " bytes) to " + address
+                        + ":" + port + ". Protocol TCP");
+                out2.writeInt((int) fileSize); //Send file size first as a 32bit number (Max size 2GB)
+                int fileBuffer = bufferInformation.getFileReadingBuffer();
+                int fileInputBuffer = bufferInformation.getFileInputBuffer();
+                long[] latencies = new long[times];
+                for (int i = 0; i < times; i++) {
+                    latencies[i] = singleFile(fileToSend,out,in, fileBuffer, fileInputBuffer);
+                    Thread.sleep(interval);
+                }
+                System.out.println("File load test finished");
+                double avgLatency = this.addAverageLatency(latencies);
+                System.out.println("Average latency: " + avgLatency + " ns. Times that the File was sent = "
+                        + times);
+                fileToSend.deleteOnExit();
+                break;
+            } catch(SocketException e) {
+                connectionAttempts++;
+                if (connectionAttempts <= MAX_RECONNECTION_ATTEMPTS){
+                    System.out.println("Trying again");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException ex) {
+                        throw new TCPConnectionException("Interrupted while reconnecting", ex);
+                    }
+                    continue;
+                }
+                throw new TCPConnectionException("Server cannot be reached", e);
             }
-            System.out.println("File load test finished");
-            double avgLatency = this.addAverageLatency(latencies);
-            System.out.println("Average latency: " + avgLatency + " ns. Times that the File was sent = "
-                    + times);
-            fileToSend.deleteOnExit();
-        } catch (IOException | InterruptedException e) {
-            throw new TCPConnectionException("TCP Connection failed: ",e);
-        }
+            catch (IOException | InterruptedException e) {
+                throw new TCPConnectionException("TCP Connection failed: ",e);
+            }
+        } while (true);
     }
 
     private void sendVideo() {
